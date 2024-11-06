eca2becb3b1a399233de2688bcf77beec82bea9d
tbone <tufytop@gmail.com>
2024-11-05 20:58:48 -0800

    add all wallets view

--- Django_Web3_Aggregator/wallet_analyzer/views.py
+++ Django_Web3_Aggregator/wallet_analyzer/views.py
@@ -1,3 +1,24 @@
+from lib2to3.fixes.fix_input import context
+
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views.generic import TemplateView, ListView
+
+from .models import Wallet
 
 # Create your views here.
+
+
+class WalletAnalyzerView(TemplateView):
+    template_name = "wallet_analyzer/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['wallets'] = Wallet.objects.all()
+        return context
+
+
+class WalletAnalyzerListView(ListView):
+    template_name = "wallet_analyzer/index.html"
+    model = Wallet
+    context_object_name = "wallets"
