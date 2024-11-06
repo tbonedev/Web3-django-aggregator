6625ad94d58dcad9919e3ae1e5bfcc74e914671e
tbone <tufytop@gmail.com>
2024-11-05 20:56:41 -0800

    add model Wallet

--- Django_Web3_Aggregator/wallet_analyzer/models.py
+++ Django_Web3_Aggregator/wallet_analyzer/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Wallet(models.Model):
+    class Meta:
+        ordering = ('id', )
+        verbose_name = 'Wallet'
+    address = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return f"{self.address}"
