9a148104532bc1c8c1e5bde49dd670d1765b5594
tbone <tufytop@gmail.com>
2024-11-05 20:56:01 -0800

    update admin panel

--- Django_Web3_Aggregator/wallet_analyzer/admin.py
+++ Django_Web3_Aggregator/wallet_analyzer/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from wallet_analyzer.models import Wallet
+
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ('id', "address")
+    list_display_links = ('id', "address")
