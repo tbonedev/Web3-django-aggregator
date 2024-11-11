c5e8eb19d2681cf68a3f44346942e38540ee4aca
tbone <tufytop@gmail.com>
2024-11-10 17:43:25 -0800

    create config for wallet-analysis

---
+++ Django_Web3_Aggregator/wallet_analyzer/wallet_analysis/сonfig.py
@@ -0,0 +1,25 @@
+# config.py
+from web3 import Web3
+
+class EVMConnection:
+    networks = {
+        "ethereum_mainnet": "https://eth.llamarpc.com",
+    }
+
+    def __init__(self, network_name):
+        self.network_name = network_name
+        self.web3 = Web3(Web3.HTTPProvider(self.networks[network_name]))
+
+        # Пример использования функции и вывода результата
+        connection_status = self.is_connected()
+        print("Подключение установлено" if connection_status else "Ошибка подключения")
+
+    def is_connected(self):
+        return self.web3.is_connected()
+
+    def get_web3_instance(self):
+        return self.web3
+
+# Автоматическое создание экземпляра при импорте файла
+eth_connection = EVMConnection
+
