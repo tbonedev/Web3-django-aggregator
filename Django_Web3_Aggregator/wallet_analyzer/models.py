4575bcf06ed952f13c05ca7ae46fb50977f31e14
tbone <tufytop@gmail.com>
2024-11-11 19:13:44 -0800

    change models + new migration

--- Django_Web3_Aggregator/wallet_analyzer/models.py
+++ Django_Web3_Aggregator/wallet_analyzer/models.py
@@ -2,10 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Wallet(models.Model):
+    address = models.CharField(max_length=255)
+
+
     class Meta:
         ordering = ('id', )
         verbose_name = 'Wallet'
-    address = models.CharField(max_length=255)
 
     def __str__(self) -> str:
         return f"{self.address}"
